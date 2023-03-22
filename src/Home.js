a6f29f27fe8c87b42163d21e843d1343f8bab4ab
josephlu2 <97142274+josephlu2@users.noreply.github.com>
2023-03-21 22:51:52 -0400

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const Home = () => {
+    // let name = "mario";
+    const [name, setName] = useState('mario')
+
+    const handleClick = () => {
+        setName('luigi')
+    }
+
+    return (
+        <div className = "home">
+            <h2>Homepage</h2>
+            <p>{ name }</p>
+            <button onClick = {handleClick}>Click me</button>
+        </div>
+    )
+}
+
+export default Home;
