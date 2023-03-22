20b937dc21a36cda9217a81e69004436e251fe91
josephlu2 <97142274+josephlu2@users.noreply.github.com>
2023-03-21 22:45:50 -0400

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('catalyst', e)
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name);
+    }
+
+    return (
+        <div className = "home">
+            <h2>Homepage</h2>
+            <button onClick = {handleClick}>Click me</button>
+            <button onClick = {() => {handleClickAgain('someone')}}>Click me again</button>
+        </div>
+    )
+}
+
+export default Home;
