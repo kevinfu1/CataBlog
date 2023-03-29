fc067455616b589c74fb12e6deb51608ec95420f
kevin <kevinwenfu@gmail.com>
2023-03-28 22:04:08 -0400

    update

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      { title: 'Blockchain is the Future', body: 'lorem ipsum...', author: 'Bharat', id: 1 },
+      { title: 'Fratalyst', body: 'lorem ipsum...', author: 'Frankie', id: 2 },
+      { title: 'How to become a Leetcode God', body: 'lorem ipsum...', author: 'Lavish', id: 3 }
     ]);
 
 
