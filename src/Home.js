9514f9c3d7b539464b0607d139b1c8a0d01f1bd4
kevin <kevinwenfu@gmail.com>
2023-03-28 22:47:12 -0400

    lesson 12

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,18 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      { title: 'Blockchain is the Future', body: 'lorem ipsum...', author: 'Bharat', id: 1 },
+      { title: 'Fratalyst', body: 'lorem ipsum...', author: 'Frankie', id: 2 },
+      { title: 'How to become a Leetcode God', body: 'lorem ipsum...', author: 'Lavish', id: 3 },
     ]);
 
 
     return (
-        <div className = "home">
-          <BlogList blogs={blogs} title="All Blogs!"/>
-        </div>
-    )
+      <div className='home'>
+        <BlogList blogs={blogs} title='All Blogs!' />
+        <BlogList blogs={blogs.filter((blog)=> blog.author === 'Frankie')} title="Frankie's Blogs!" />
+      </div>
+    );
 }
 
 export default Home;
