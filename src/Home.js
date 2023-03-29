53630f78dd8f2bd9afe42f120abcd5bbb537351b
kevin <kevinwenfu@gmail.com>
2023-03-28 21:38:33 -0400

    event object

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,15 @@ const Home = () => {
         console.log('catalyst', e)
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
     }
 
     return (
         <div className = "home">
             <h2>Homepage</h2>
             <button onClick = {handleClick}>Click me</button>
-            <button onClick = {() => {handleClickAgain('someone')}}>Click me again</button>
+            <button onClick = {(e) => {handleClickAgain('someone', e)}}>Click me again</button>
         </div>
     )
 }
