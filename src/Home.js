38f37f765e24d3a4101afb131ac51bfdcdb0ff9f
kevin <kevinwenfu@gmail.com>
2023-03-28 23:57:06 -0400

    lesson 20

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,9 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-        .then((res) => {
-          console.log(res);
-          if (!res.ok) {
-            throw Error('Could not fetch the data for that resource');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          console.log(err.message);
-          setError(err.message);
-          setIsLoading(false)
-        });
-    }, 1000);
-  }, []);
+  const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className='home'>
