191513d9984a1362456a130bb4abd2f7bb5f49d8
kevin <kevinwenfu@gmail.com>
2023-03-28 23:04:48 -0400

    lesson 15

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-      { title: 'Blockchain is the Future', body: 'lorem ipsum...', author: 'Bharat', id: 1 },
-      { title: 'Fratalyst', body: 'lorem ipsum...', author: 'Frankie', id: 2 },
-      { title: 'How to become a Leetcode God', body: 'lorem ipsum...', author: 'Lavish', id: 3 },
-    ]);
+  const [blogs, setBlogs] = useState([
+    { title: 'Blockchain is the Future', body: 'lorem ipsum...', author: 'Bharat', id: 1 },
+    { title: 'Fratalyst', body: 'lorem ipsum...', author: 'Frankie', id: 2 },
+    { title: 'How to become a Leetcode God', body: 'lorem ipsum...', author: 'Lavish', id: 3 },
+  ]);
 
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs);
-    }
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
-    return (
-      <div className='home'>
-        <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
-      </div>
-    );
-}
+  useEffect(() => {
+    console.log('useEffect ran');
+    console.log(blogs);
+  });
+
+  return (
+    <div className='home'>
+      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
+    </div>
+  );
+};
 
 export default Home;
